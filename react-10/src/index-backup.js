29c60a4598dc42db0474d197db80bd6d82bb7ba4
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-10-11 11:39:33 +0530

    okie

---
+++ react-10/src/index-backup.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+//ReactDOM.render("Hello,Rahul", document.getElementById('rajni'));
+ReactDOM.render(<><h1>Hello,Rahul</h1><h2>welcome to KA</h2></>,
+    document.getElementById('rajni'));
+//ReactDOM.render(1,2);
+//
+//ReactDOM.render("What you want to render","where you want to render");
---
+++ react-10/src/index-backup1.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+//ReactDOM.render("Hello,GM", document.getElementById('root'));
+//ReactDOM.render(<h1>Hello,GM</h1>, document.getElementById('root'));
+ReactDOM.render(<>
+
+</>, document.getElementById('root'));
+//ReactDOM.render(<div><h1>Hello,GM</h1><p>loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem </p></div>, document.getElementById('root'));
